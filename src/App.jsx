26847249535fc323c3220a7c9a0644ab0e60f043
abrahan <abrahandev@gmail.com>
2023-07-13 08:23:56 +0200

    App.jsx modificada

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 function App() {
 
-  const edad = 18
+ 
     return (
       <>
-      {edad >= 18 ? 'Eres mayor de edad' : 'No eres mayor'}
-       <div>
-         <h1>{'Hola mundo'.toUpperCase()}</h1>
-       </div>
+   
       </>
     )
 }
