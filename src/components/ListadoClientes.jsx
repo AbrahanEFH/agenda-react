f43e3188024194db5f681a012a24663d1e5a23f9
abrahan <abrahandev@gmail.com>
2023-07-14 16:45:49 +0200

    listado de componentes terminado

--- src/components/ListadoClientes.jsx
+++ src/components/ListadoClientes.jsx
@@ -2,13 +2,18 @@ import Cliente from "./Cliente"
 
 const ListadoClientes = () => {
   return (
-    <div className=" w-1/2 lg:w-3/5 " >
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll " >
         <h2 className=" font-black text-3xl text-center" >Listado Clientes</h2>
         <p className=" text-xl mt-5 mb-10 text-center">
             Administra tus {''}
             <span className=" text-indigo-400 font-bold " >Clientes y Citas</span>
         </p>
 
+          <Cliente />
+          <Cliente />
+          <Cliente />
+          <Cliente />
+          <Cliente />
           <Cliente />
   
     </div>
