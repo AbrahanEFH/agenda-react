f9341a879e0973bfaf6e9a66eceefd3afa99f07c
abrahan <abrahandev@gmail.com>
2023-07-23 14:48:09 +0200

    Destruturing aplicado para tomar los datos del formulario

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,26 +1,28 @@
 
 const Cliente = ({cliente}) => {
-    console.log(cliente)
+
+    const { nombre, apellido, email, cita, servicio } = cliente
+
   return (
      <div className=" my-8 mx-5 bg-white shadow-md px-5 py-5 rounded-xl">
         <p className=" font-bold mb-3 text-gray-700 uppercase"> Nombre: {''}
-            <span className=" font-normal normal-case">Cliente</span>
+            <span className=" font-normal normal-case">{nombre}</span>
 
         </p>
         <p className=" font-bold mb-3 text-gray-700 uppercase"> Apellido: {''}
-            <span className=" font-normal normal-case">Clientazo</span>
+            <span className=" font-normal normal-case">{apellido}</span>
 
         </p>
         <p className=" font-bold mb-3 text-gray-700 uppercase"> Email: {''}
-            <span className=" font-normal normal-case">correo@correo.co</span>
+            <span className=" font-normal normal-case">{email}</span>
 
         </p>
         <p className=" font-bold mb-3 text-gray-700 uppercase"> Fecha: {''}
-            <span className=" font-normal normal-case">10-10-23</span>
+            <span className=" font-normal normal-case">{cita}</span>
 
         </p>
         <p className=" font-bold mb-3 text-gray-700 uppercase"> Servicio: {''}
-            <span className=" font-normal normal-case">Hidratacion y alisado</span>
+            <span className=" font-normal normal-case">{servicio}</span>
 
         </p>
     </div>
